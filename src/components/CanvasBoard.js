932bd3f295eb0825e8f40a8d5a21b80fa4c8c163
Vincent Sanders <vincesanders@hotmail.com>
2020-05-28 10:25:23 -0500

    Refactored CanvasBoard.

--- src/components/CanvasBoard.js
+++ src/components/CanvasBoard.js
@@ -8,83 +8,79 @@ class CanvasBoard extends React.Component {
     }
     
     componentDidMount(){
-        var canvas = document.getElementById('canvas-board')
-        var ctx = canvas.getContext('2d')
-        this.ctx = ctx
-        this.drawCells()
+        var canvas = document.getElementById('canvas-board');
+        var ctx = canvas.getContext('2d');
+        this.ctx = ctx;
+        this.drawCells();
     }
-    
+    //only update when necessary - OPTIMIZED
     shouldComponentUpdate(nextProps, nextState){
-    
-        if (nextProps.gridIsOn != this.props.gridIsOn) { return true }
-        if (nextProps.changeBuffer.length != this.props.changeBuffer.length) { return true }
-        if (nextProps.changeBuffer && 
-            nextProps.changeBuffer.some((c, i) => c != this.props.changeBuffer[i])) { 
-            return true 
+        if (nextProps.gridIsOn != this.props.gridIsOn) { return true; }
+        if (nextProps.changeBuffer.length != this.props.changeBuffer.length) { return true; }
+        if (nextProps.changeBuffer && nextProps.changeBuffer.some((c, i) => c != this.props.changeBuffer[i])) { 
+            return true;
         }
-        if (nextProps.grid.length &&
-            nextProps.grid.every(c => c == 0) ) {
-            return true 
+        if (nextProps.grid.length && nextProps.grid.every(c => c == 0) ) {
+            return true;
         }
-        if(!nextProps.grid.length &&
-            !this.props.grid.length) { 
-            return false 
+        if(!nextProps.grid.length && !this.props.grid.length) { 
+            return false;
         }
-        if (nextProps.grid.length !=
-            this.props.grid.length ||
-            nextProps.grid.some((c, i) => this.props.grid[i] != c)) {
-            return true
+        if (nextProps.grid.length != this.props.grid.length || nextProps.grid.some((c, i) => this.props.grid[i] != c)) {
+            return true;
         }
-        return false
+        return false;
     }
     
     componentWillUpdate(nextProps) {
-        this.drawCells(nextProps)   
+        this.drawCells(nextProps);
     }
     
-    drawCells(props=this.props){
-        var grid = props.grid
-        var changeBuffer = props.changeBuffer
-        if(!grid.length) { return }
-        var liveCell = '#EB891A'
-        var deadCell = '#030201'
-        var ctx = this.ctx
-        var size = this.props.cellSize
-        var _this = this
+    drawCells(props=this.props) {
+        const grid = props.grid;
+        const changeBuffer = props.changeBuffer;
+        if (!grid.length) { 
+            return;
+        }
+        const liveCell = '#EB891A';
+        const deadCell = '#030201';
+        const ctx = this.ctx;
+        const size = this.props.cellSize;
+        const that = this;
 
         const fillCell = (index, fill) => {
             // cell filling function
-            var c = (index % _this.props.cols) * size
-            var r = ((index - (index % _this.props.cols)) / _this.props.cols) * size
-            ctx.fillStyle = fill
-            ctx.fillRect(c, r, size, size) 
+            const c = (index % that.props.cols) * size;
+            const r = ((index - (index % that.props.cols)) / that.props.cols) * size;
+            ctx.fillStyle = fill;
+            ctx.fillRect(c, r, size, size);
         }
         
         if (!changeBuffer.length){
             // if reading from .grid because first turn
             grid.forEach((cell, i) => {
-                if (cell == 1) {
-                fillCell(i, liveCell)
-                } else if (cell == 0){
-                fillCell(i, deadCell)
+                if (cell === 1) {
+                    fillCell(i, liveCell);
+                } else if (cell === 0){
+                    fillCell(i, deadCell);
                 }
-            })
+            });
         } else {
             // if reading from changeBuffer
             changeBuffer.forEach(i => {
-            if (grid[i] == 1) { 
-                fillCell(i, liveCell)
-                } else if (grid[i] == 0) { 
-                fillCell(i, deadCell)
+            if (grid[i] === 1) { 
+                    fillCell(i, liveCell);
+                } else if (grid[i] === 0) { 
+                    fillCell(i, deadCell);
                 }
-            })
+            });
         }
     }
     
     render() {
-        var size = this.props.cellSize
-        var width = this.props.cols * size
-        var height = this.props.rows * size
+        const size = this.props.cellSize;
+        const width = this.props.cols * size;
+        const height = this.props.rows * size;
         
         return (
             <div id="canvas-container">
